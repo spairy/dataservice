fc3c64dd9afb63d79d986581370d833c8f72aa9e
sunranhou <sunranhou@126.com>
2016-04-12 01:08:46 +0800

    test

---
+++ src/main/java/com/sun/yong/dataservice/App.java
@@ -0,0 +1,13 @@
+package com.sun.yong.dataservice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/sun/yong/dataservice/impl/DataServiceImpl.java
@@ -0,0 +1,14 @@
+package com.sun.yong.dataservice.impl;
+
+import com.sun.yong.common.entity.model.UserInfo;
+import com.sun.yong.common.exception.DateServiceException;
+import com.sun.yong.dataservice.IDataService;
+
+public class DataServiceImpl implements IDataService {
+
+	public UserInfo getUserInfo() throws DateServiceException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
